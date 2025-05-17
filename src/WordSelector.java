1f9fb40b0b67506a34b825bbd432fd0ad759567a
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-05-17 20:56:26 +0500

    add methods checkLetter and repeatLetter

--- src/WordSelector.java
+++ src/WordSelector.java
@@ -1,14 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class WordSelector {
 
-    //обработка на любой некорректный ввод  при запуске/кроме а или в
-    // обработка длины буквы
-    //и запрос на повтор игры
+    //обработка на любой некорректный ввод  при запуске/кроме а или в = +
+    // обработка длины буквы = + )
+    //и запрос на повтор игры =
 
-    public void  playGame(String word, HangmanDrawer drawer, GameView view, Scanner sc){
+    public void  playGame(String word, HangmanDrawer drawer, GameView view, Scanner sc) {
         System.out.println("\tИгра начинается ^_^");
         System.out.println("\tСлово Загадано!\n");
 
@@ -16,15 +14,28 @@ public class WordSelector {
         lockWord.append(word.replaceAll("[а-яА-Я]", "_"));
 
         List<String> errorsList = new ArrayList<>();
+        Set<String> lettersList = new HashSet<>();
         int countError = 0;
-        int tryCount =6;
+        int tryCount = 6;
 
 
-        while (!lockWord.toString().equalsIgnoreCase(word) && countError!=6) {
+        while (!lockWord.toString().equalsIgnoreCase(word) && countError != 6) {
             System.out.println("\nВведите букву: ");
 
-           String letter = sc.nextLine();
-           checkLetter(letter);
+
+            String letter = sc.nextLine();
+
+            while (checkLetter(letter)) {
+                System.out.println("Введите Одну букву!");
+                letter = sc.nextLine();
+            }
+
+            if(repeatLetter(lettersList, letter)){
+                System.out.println("Эта буква уже была! Введите другую букву");
+                letter = sc.nextLine();
+            }
+            lettersList.add(letter);
+
 
             boolean found = false;
             for (int i = 0; i < word.length(); i++) {
@@ -38,20 +49,25 @@ public class WordSelector {
                 countError++;
                 tryCount--;
             }
-            drawer.draw(countError);
-            view.playInfo(lockWord, errorsList, countError,tryCount, letter);
+                drawer.draw(countError);
+                view.playInfo(lockWord, errorsList, countError, tryCount, letter);
 
+            }
+
+            if (lockWord.toString().equalsIgnoreCase(word)) {
+                view.winGameMessage(lockWord, word);
+            } else {
+                view.overGameMessage(countError, word);
+            }
         }
-        if(lockWord.toString().equalsIgnoreCase(word)){
-            view.winGameMessage(lockWord, word);
-    } else {
-          view.overGameMessage(countError, word);
+
+        public boolean checkLetter(String letter){
+       return letter.length()>1;
+        }
+
+        public boolean repeatLetter(Set<String> letterList,String letter){
+       return letterList.contains(letter);
         }
         }
 
-        public void checkLetter(String letter){
-        if(letter.length()>1){
-            System.out.println("Некорректный ввод! Введите одну букву!");
-        }
-    }
-}
+
